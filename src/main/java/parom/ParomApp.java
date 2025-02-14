511fb2668cd821f885b51bde050e673040991f51
IlyaYakubski <yakubski.ilya@student.ehu.lt>
2025-02-14 14:02:06 +0200

    aa

--- src/main/java/parom/ParomApp.java
+++ src/main/java/parom/ParomApp.java
@@ -6,6 +6,7 @@ import parom.service.ParomService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
